18b557241766741864988dbf5545718942d73854
Mason Soroka-Gill <SOROM2@student.op.ac.nz>
2021-05-19 22:18:09 +1200

    change default to accept connections on all interfaces

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char* argv[])
 {
     const std::string docRoot= "docroot";
-    const std::string host = "127.0.0.1";
+    const std::string host = "0.0.0.0";
     const unsigned short port = 8080;
     const unsigned int threads = 0; // 0 defaults to max hardware threads
 
