f3ebcf719be0357aa480b9cfad3bef518df63826
Mason <SOROM2@student.op.ac.nz>
2021-12-04 17:36:04 +1300

    Add a redirect to the default route to direct to '/', move welcome to '/', align welcome response so it's not tabbed out

--- examples/hello_world.cpp
+++ examples/hello_world.cpp
@@ -12,21 +12,30 @@ void hello_world(hus::Request& req, hus::Response& resp)
 void welcome(hus::Request& req, hus::Response& resp)
 {
     resp.body = R"(
-        <h1>Welcome to a new world!</h1>
-        <p>Click <a href="/hello">HERE</a> to go to the hello world, or fill in and submit the form below</p><br>
-        <form action="/hello">
-        <label for="name">Enter your name: </label>
-        <input type="text" id="name" name="name" required>
-        <input type="submit" value="Submit">
-        </form>
+
+<h1>Welcome to a new world!</h1>
+<p>Click <a href="/hello">HERE</a> to go to the hello world, or fill in and submit the form below</p><br>
+<form action="/hello">
+<label for="name">Enter your name: </label>
+<input type="text" id="name" name="name" required>
+<input type="submit" value="Submit">
+</form>
+
     )";
+}
+
+void redirect(hus::Request& req, hus::Response& resp)
+{
+    resp.code = "308";
+    resp.Headers["Location"] = "/";
 }
 
 int main()
 {
     hus::Hussar h("127.0.0.2", 8443, 0, "key.pem", "cert.pem", true);
 
-    h.Router.DEFAULT(&welcome);
+    h.Router.DEFAULT(&redirect);
+    h.Router.GET("/", &welcome);
     h.Router.GET("/hello", &hello_world);
 
     h.Listen();
